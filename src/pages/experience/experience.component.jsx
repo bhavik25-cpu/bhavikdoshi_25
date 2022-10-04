8b231644650c86a3886c10075ab906427b5d41bc
bhavik doshi <bhavikdoshi25@gmail.com>
2022-10-04 15:10:37 +0530

    first commit!

--- src/pages/experience/experience.component.jsx
+++ src/pages/experience/experience.component.jsx
@@ -3,6 +3,7 @@ import Container from "react-bootstrap/Container";
 import Jumbotron from "react-bootstrap/Jumbotron";
 import Card from "react-bootstrap/Card";
 import L_vernost from "../../assets/img/experience/vernost.jpeg";
+import L_gupshup from "../../assets/img/experience/gupshup.jpeg";
 import Tilt from "react-tilt";
 import "./experience.styles.css";
 
@@ -48,11 +49,51 @@ const Experience = () => {
                 </div>
               </Card.Body>
             </Card>
+
+            <Card>
+              <Card.Header as="h5" className="d-flex justify-content-center flex-wrap">
+                <Card.Img variant="top" className="img-resize" src={L_gupshup} alt="Accenture logo" />
+              </Card.Header>
+              <Card.Body className="d-flex justify-content-center flex-column">
+                <div>
+                  <Card.Title className="text-center">Associate Software Engineer</Card.Title>
+                </div>
+                <div>
+                  <Card.Text className="text-center style">
+                    <strong className="body-title-style ">Backend Developer</strong>
+                    <br />
+                    <strong>Technology:</strong> React JS, javascript, mysql workbench, Node JS 
+                    <br />
+                    <strong>Duration:</strong> May 2020- Present
+                    <br/>
+                    {/* <strong> Description </strong> */}
+                    <ul className="text-left">
+                      {/* <li><strong>Developed &amp; enhanced</strong> multiple features with customizability option across web application.</li>
+                      <li><strong>Developed</strong> automation system to create SQL bulk query scripts that 
+                        increased efficiency by 80% and decreased working hours from 4 hours to 30 mins per task.
+                      </li>
+                      <li><strong>Provided</strong> application maintenance while working as `Production Support`. 
+                      </li>
+                      <li><strong>Performed</strong> CRUD operations on multiple databases to load/ remove data according 
+                      to the business requirements.</li> */}
+                      {/* <li><strong>Co-created</strong> React Document used as a guide for new developers.</li> */}
+
+                    </ul>
+                    
+                    
+                  </Card.Text>
+                </div>
+              </Card.Body>
+            </Card>
           </Tilt>
         </Container>
       </Jumbotron>
     </div>
+
+    
   );
 };
+
+
 
 export default Experience;
